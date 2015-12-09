82bfbe44a3598f21ece51b1e2a234b59f80f74ab
Huw <hdrdavies@gmail.com>
2015-12-09 14:45:18 +0000

    attempting to use cluster markers #40

--- views/map.js
+++ views/map.js
@@ -52,6 +52,8 @@
          httpRequest.send();
      }
 
+     var markers = [];
+
      fetchJSONFile('/data', function(data) {
          // do something with your data
          data = JSON.parse(data);
@@ -61,6 +63,7 @@
                  position: new google.maps.LatLng(Number(data[key].latitude), Number(data[key].longitude))
              });
              marker.setMap(map);
+             markers.push(marker);
 
             // Add Infowindow
              var infowindow = new google.maps.InfoWindow({
@@ -73,6 +76,8 @@
          });
      });
 
+     console.log(markers);
+
      // Search Box
      var input = document.getElementById('pac-input');
      var searchBox = new google.maps.places.SearchBox(input);
@@ -82,47 +87,48 @@
          searchBox.setBounds(map.getBounds());
      });
 
-     var markers = [];
-
-     searchBox.addListener('places_changed', function() {
-         var places = searchBox.getPlaces();
-
-         if (places.length === 0) {
-             return;
-         }
-
-         // Clear out the old markers.
-         markers.forEach(function(marker) {
-             marker.setMap(null);
-         });
-         markers = [];
-
-         // For each place, get the icon, name and location.
-         var bounds = new google.maps.LatLngBounds();
-         places.forEach(function(place) {
-             var icon = {
-                 url: place.icon,
-                 size: new google.maps.Size(71, 71),
-                 origin: new google.maps.Point(0, 0),
-                 anchor: new google.maps.Point(17, 34),
-                 scaledSize: new google.maps.Size(25, 25)
-             };
-
-             // Create a marker for each place.
-             markers.push(new google.maps.Marker({
-                 map: map,
-                 icon: icon,
-                 title: place.name,
-                 position: place.geometry.location
-             }));
-
-             if (place.geometry.viewport) {
-                 // Only geocodes have viewport.
-                 bounds.union(place.geometry.viewport);
-             } else {
-                 bounds.extend(place.geometry.location);
-             }
-         });
-         map.fitBounds(bounds);
-     });
+    //  var markers = [];
+     //
+    //  searchBox.addListener('places_changed', function() {
+    //      var places = searchBox.getPlaces();
+     //
+    //      if (places.length === 0) {
+    //          return;
+    //      }
+     //
+    //      // Clear out the old markers.
+    //      markers.forEach(function(marker) {
+    //          marker.setMap(null);
+    //      });
+    //      markers = [];
+     //
+    //      // For each place, get the icon, name and location.
+    //      var bounds = new google.maps.LatLngBounds();
+    //      places.forEach(function(place) {
+    //          var icon = {
+    //              url: place.icon,
+    //              size: new google.maps.Size(71, 71),
+    //              origin: new google.maps.Point(0, 0),
+    //              anchor: new google.maps.Point(17, 34),
+    //              scaledSize: new google.maps.Size(25, 25)
+    //          };
+     //
+    //          // Create a marker for each place.
+    //          markers.push(new google.maps.Marker({
+    //              map: map,
+    //              icon: icon,
+    //              title: place.name,
+    //              position: place.geometry.location
+    //          }));
+     //
+    //          if (place.geometry.viewport) {
+    //              // Only geocodes have viewport.
+    //              bounds.union(place.geometry.viewport);
+    //          } else {
+    //              bounds.extend(place.geometry.location);
+    //          }
+    //      });
+    //      map.fitBounds(bounds);
+    //  });
+     var mc = new MarkerClusterer(map, markers);
  }
