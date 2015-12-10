21d87a2dd39ebb9885c848a7a2908bc66f8e991b
katkelemen <kelemenkata@gmail.com>
2015-12-10 21:48:41 +0000

    #53 #41 other infowindows closing and map.js is more modularized, see util.js

---
+++ views/util.js
@@ -0,0 +1,47 @@
+utils = {
+  fetchJSONFile: function (path, callback) {
+      var httpRequest = new XMLHttpRequest();
+      httpRequest.onreadystatechange = function() {
+          if (httpRequest.readyState === 4) {
+              if (httpRequest.status === 200) {
+                  var data = httpRequest.responseText;
+                  if (callback) callback(data);
+              }
+          }
+      };
+      httpRequest.open('GET', path);
+      httpRequest.send();
+  },
+
+  setupInitialLocation: function(map) {
+    if (navigator.geolocation) {
+        browserSupportFlag = true;
+        navigator.geolocation.getCurrentPosition(function(position) {
+            initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+            map.setCenter(initialLocation);
+        }, function() {
+            initialLocation = new google.maps.LatLng(51.508742, -0.120850);
+        });
+    } else {
+      initialLocation = new google.maps.LatLng(51.508742, -0.120850);
+    }
+  },
+
+  renderInfoContent: function(data, key){
+    return '<div id="name">'+
+              '<h3>'+data[key].Name+'</h3>'+
+              '</div>'+
+              '<div id="website">'+
+              '<p hidden>'+data[key].Website+'</p>'+
+              '</div>'+
+              '<div id="rampDes">'+
+              '<p hidden>'+data[key].RampDescription+'</p>'+
+              '</div>'+
+              '<div id="charges">'+
+              '<p hidden>'+data[key].Charges+'</p>'+
+              '</div>'+
+              '<button onclick="myFunc()" type="button">More Info</button>'+
+              '</div>';
+  }
+
+};
