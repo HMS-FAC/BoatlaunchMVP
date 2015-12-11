b2ffa7ad2473ce199babb504fac6a0521f2f0c4b
tormod17 <tormodsmith@gmail.com>
2015-12-11 16:44:30 -0400

    checked, search, map and slipwayInfo #77

--- tests/frontendTests.js
+++ tests/frontendTests.js
@@ -3,11 +3,14 @@ console.log('hello testing');
 var iframe = document.getElementById('iframe');
 var target = iframe.contentDocument || iframe.contentWindow.document;
 //var done = assert.async();
+var searchBox = target.getElementById('pac-input');
+var map = target.getElementById('googleMap');
+var slipwayInfo = target.getElementById('slipwayInfo');
 
 
-
-test('is searchBox there',function(assert){
-	var searchBox = target.getElementById('pac-input');
-
-	assert.ok(searchBox,true);
+test('is searchBox,map div and slipInfo div there', function(assert) {
+	assert.ok(searchBox,'it exists');
+	assert.ok(map,'it exists');
+ 	assert.ok(slipwayInfo,'it exists');
 });
+
