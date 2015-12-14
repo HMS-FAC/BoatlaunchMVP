0684b6b4a1160b82c3fffefa3c621306efdb8ee4
Huw <hdrdavies@gmail.com>
2015-12-14 17:03:20 +0000

    tidy and modularise #82

--- views/utils.js
+++ views/utils.js
@@ -7,8 +7,10 @@ utils = {
     firebaseSlipwayArray.on('value', function(snapshot) {
       return callback(snapshot.val());
     });
+
   },
   transformSlipways: function(remoteSlipways) {
+
     localSlipwayObj = {};
     var slipwayKey;
     for (slipwayKey in remoteSlipways) {
@@ -17,8 +19,10 @@ utils = {
       }
     }
     return localSlipwayObj;
+
   },
   transformSingleSlipway: function(slipwayArray) {
+
     if (typeof slipwayArray[2] === 'number') {
       slipwayArray[2] = slipwayArray[2].toString();
     }
@@ -53,58 +57,12 @@ utils = {
       'CruisingArea':       slipwayArray[22],
       'NavigationalHazards':slipwayArray[23],
       'images':             slipwayArray[24]
+
     };
   },
-  submitNewSlipwayDetails: function(data){
-    var idKey = Date.now();
-    var keyRoute = new Firebase('https://crackling-inferno-1794.firebaseio.com/'+ idKey);
-    var nameInput = document.getElementById('Name').value,
-        longInput = document.getElementById('Longitude').value,
-        latInput = document.getElementById('Latitude').value,
-        websiteInput = document.getElementById('Website').value,
-        personInput = document.getElementById('PersonName').value,
-        phoneNumberInput = document.getElementById('PhoneNumber').value,
-        mobilePhoneInput = document.getElementById('MobilePhoneNumber').value,
-        emailInput = document.getElementById('Email').value,
-        rampDescInput = document.getElementById('RampDescription').value,
-        rampTypeInput = document.getElementById('RampType').value,
-        suitabilityInput = document.getElementById('Suitability').value, // select
-        rampLengthInput = document.getElementById('RampLength').value, // select
-        facilitiesInput = document.getElementById('Facilities').value,
-        navHazInput = document.getElementById('NavigationalHazards').value,
-        upperAreaMaterialInput = document.getElementById('UpperAreaMaterial').value, // select
-        lowerAreaMaterialInput = document.getElementById('LowerAreaMaterial').value, //select
-        chargesInput = document.getElementById('Charges').value;
 
-    keyRoute.set([
-      idKey,
-      nameInput,
-      longInput,
-      latInput,
-              , // NearestPlace
-              , // Country
-              , // Region
-      websiteInput,
-      personInput,
-      phoneNumberInput,
-      mobilePhoneInput,
-              , // Fax
-      emailInput,
-      rampDescInput,
-              , // Directions
-      rampTypeInput,
-      upperAreaMaterialInput,
-      lowerAreaMaterialInput,
-      suitabilityInput,
-      rampLengthInput,
-      facilitiesInput,
-      chargesInput,
-                  , // CruisingArea
-      navHazInput,
-                    // Images
-    ]);
-  },
   setupInitialLocation: function(map) {
+
     if (navigator.geolocation) {
       browserSupportFlag = true;
       navigator.geolocation.getCurrentPosition(function(position) {
