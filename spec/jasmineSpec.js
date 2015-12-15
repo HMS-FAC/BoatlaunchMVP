5e40867c47847d95971ebb1177b6f658e70e4dab
Huw <hdrdavies@gmail.com>
2015-12-15 15:16:15 +0000

    tidy up test functions #92

--- spec/jasmineSpec.js
+++ spec/jasmineSpec.js
@@ -1,23 +1,28 @@
-describe('Test Jasmine', function(){
-  it('simple sum', function(){
-
-    expect(1+1).toEqual(2);
-  });
-
-});
-
 describe('Checking utils.renderInfoContent', function(){
+  var data = {
+    123: {
+      Name: "Huw"
+    }
+  };
+  var key = 123;
+
   it('returns HTML string', function(){
-    var data = {
-      123: {
-        Name: "Huw"
-      }
-    };
-    var key = 123;
     var htmlString = utils.renderInfoContent(data, key);
     var type = typeof htmlString;
 
     expect(type).toEqual('string');
+  });
 
+  it('returns expected template', function (){
+    var expected = '<div id="name">' +
+      '<h3>' + data[key].Name + '</h3>' +
+      '</div>' +
+      '<p hidden id="key">'+key+'</p hidden>'+
+
+      '<button onclick="showSlipwayDetails()" type="button">More Info</button>' +
+      '</div>';
+    var result = utils.renderInfoContent(data, key);
+
+    expect(result).toEqual(expected);
   });
 });
