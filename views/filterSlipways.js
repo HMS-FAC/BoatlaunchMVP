d484b98ea2e1e5589e3096bebcbb149333615287
katkelemen <kelemenkata@gmail.com>
2015-12-15 21:14:49 +0000

    #73 filter somewhat implemented, going to raise subissue

--- views/filterSlipways.js
+++ views/filterSlipways.js
@@ -11,12 +11,69 @@ $(document).ready(function(){
   // });
 
 
-
   $("#submit-filter").click(function(){
+    //show unknown slipways too
+    if($('#unknown-slipways').is(':checked')) {
+      //every slipway
+      if($('#portable').is(':checked')) {
+        markers.forEach(function(e) {
+            e.setVisible(true);
+        });
+        mc.repaint();
+        }
+      // small, large and unknown
+      if($('#small').is(':checked')) {
+        markers.forEach(function(e) {
+          if(e.__filtervalue.Suitability !== 'Small trailer can be pushed' ||
+            e.__filtervalue.Suitability !== 'Large trailer needs a car' ||
+            e.__filtervalue.Suitability !== 'Unknown'){
+              e.setVisible(false);
+          }
+        });
+        mc.repaint();
+      }
+      //large and unknown
+      if($('#large').is(':checked')) {
+        markers.forEach(function(e) {
+          if(e.__filtervalue.Suitability !== 'Large trailer needs a car' ||
+            e.__filtervalue.Suitability !== 'Unknown'){
+              e.setVisible(false);
+          }
+        });
+        mc.repaint();
+      }
+    }
+    //doesn't show unknown slipways
+    else {
+      //every slipway except unknown
+      if($('#portable').is(':checked')) {
+        markers.forEach(function(e) {
+          if(e.__filtervalue.Suitability === 'Unknown') {
+            e.setVisible(false);
+          }
+        });
+        mc.repaint();
+        }
+      //only small and large
+      if($('#small').is(':checked')) {
+        markers.forEach(function(e) {
+          if(e.__filtervalue.Suitability !== 'Small trailer can be pushed' ||
+            e.__filtervalue.Suitability !== 'Large trailer needs a car'){
+              e.setVisible(false);
+          }
+        });
+        mc.repaint();
+      }
+      //only large
+      if($('#large').is(':checked')) {
+        markers.forEach(function(e) {
+          if(e.__filtervalue.Suitability !== 'Large trailer needs a car') {
+            e.setVisible(false);
+          }
+        });
+        mc.repaint();
+      }
+    }
 
-    if($('#portable').is(':checked')) { alert("it's checked"); }
-
-    console.log('name');
   });
-
 });
--- views/main.js
+++ views/main.js
@@ -1,6 +1,7 @@
 utils.fetchRemoteSlipways(function(remoteSlipways){
   var slipways = utils.transformSlipways(remoteSlipways);
+  console.log(slipways);
 
-  createMapWithSlipways(slipways);
+  createMapWithSlipways(slipways );
 
 });
