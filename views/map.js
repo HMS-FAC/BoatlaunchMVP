82c08e6e8d5e21350a251377aa1f539a4d64a1f4
katkelemen <kelemenkata@gmail.com>
2015-12-15 14:01:34 +0000

    #73 dropdown added plus bootstrapSwitch for toggle showing unknown slipways

--- views/map.js
+++ views/map.js
@@ -36,7 +36,6 @@ function createMapWithSlipways(slipways) {
 
      var input = document.getElementById('pac-input');
      var searchBox = new google.maps.places.SearchBox(input);
-     map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
      map.addListener('bounds_changed', function() {
          searchBox.setBounds(map.getBounds());
