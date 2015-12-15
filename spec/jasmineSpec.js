94860d8cad0419118b11e17e28ec5689c18c3161
Huw <hdrdavies@gmail.com>
2015-12-15 12:54:14 +0000

    got jasmine working #92

--- spec/jasmineSpec.js
+++ spec/jasmineSpec.js
@@ -3,4 +3,21 @@ describe('Test Jasmine', function(){
 
     expect(1+1).toEqual(2);
   });
+
+});
+
+describe('Checking utils.renderInfoContent', function(){
+  it('returns HTML string', function(){
+    var data = {
+      123: {
+        Name: "Huw"
+      }
+    };
+    var key = 123;
+    var htmlString = utils.renderInfoContent(data, key);
+    var type = typeof htmlString;
+
+    expect(type).toEqual('string');
+
+  });
 });
